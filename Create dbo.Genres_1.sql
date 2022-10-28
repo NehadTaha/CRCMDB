8860e6539267d4123b4bd549ec81af6e0cab1ffd
2125697 <LE2125697@crc-lennox.qc.ca>
2022-10-28 01:06:01 -0400

    Export script table creation

---
+++ Create dbo.Genres_1.sql
@@ -0,0 +1,16 @@
+﻿USE [CRCMDB]
+GO
+
+/****** Object: Table [dbo].[Genres] Script Date: 10/28/2022 1:04:12 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Genres] (
+    [GenreID] TINYINT       IDENTITY (1, 1) NOT NULL,
+    [Genres]  NVARCHAR (50) NOT NULL
+);
+
+
