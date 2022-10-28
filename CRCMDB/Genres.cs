5107495e43174d00cca5d0f54a38847f182331cc
2125697 <LE2125697@crc-lennox.qc.ca>
2022-10-28 01:47:12 -0400

    Create new form for Genres table

---
+++ CRCMDB/Genres.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRCMDB
+{
+    public partial class Genres : Form
+    {
+        public Genres()
+        {
+            InitializeComponent();
+        }
+
+        private void genresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.genresBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.genresDataSet);
+
+        }
+
+        private void Genres_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'genresDataSet.Genres' table. You can move, or remove it, as needed.
+            this.genresTableAdapter.Fill(this.genresDataSet.Genres);
+
+        }
+    }
+}
