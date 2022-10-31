1428243b04211658c12981763b5d8df82ef61263
2125697 <LE2125697@crc-lennox.qc.ca>
2022-10-31 10:29:00 -0400

    Create MovieGenre table

---
+++ Create dbo.MovieGenre.sql
@@ -0,0 +1,17 @@
+﻿USE [C:\OOP2\CRCMDB\CRCMDB\LAB4.MDF]
+GO
+
+/****** Object: Table [dbo].[MovieGenre] Script Date: 10/31/2022 10:27:39 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MovieGenre] (
+    [MovieGenre] INT IDENTITY (1, 1) NOT NULL,
+    [GenreId]    INT NOT NULL,
+    [MovieId]    INT NOT NULL
+);
+
+
