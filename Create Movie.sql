be41ea8ee9674d0e5edd159a434592658e642279
2125697 <LE2125697@crc-lennox.qc.ca>
2022-10-31 17:47:46 -0400

    Create movies table

---
+++ Create Movie.sql
@@ -0,0 +1,18 @@
+﻿USE [C:\OOP2\CRCMDB\CRCMDB\LAB4.MDF]
+GO
+
+/****** Object: Table [dbo].[Movies] Script Date: 10/31/2022 5:45:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Movies] (
+    [MovieId] INT          IDENTITY (1, 1) NOT NULL,
+    [Title]   VARCHAR (50) NOT NULL,
+    [Year]    INT          NOT NULL,
+    [Genre]   VARCHAR (50) NOT NULL
+);
+
+
