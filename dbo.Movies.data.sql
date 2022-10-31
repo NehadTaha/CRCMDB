08d87ed694a84a8b3f89f02c2e92c929e6b1fe6d
Christophe <LE1207226@crc.lennox.qc.ca>
2022-10-31 12:06:25 -0400

    Create Movie

---
+++ dbo.Movies.data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[Movies] ([MovieId], [Title], [Year], [Genre]) VALUES (1, N'The Matrix', 1999, N'Action, Sci-Fi')
+INSERT INTO [dbo].[Movies] ([MovieId], [Title], [Year], [Genre]) VALUES (2, N'Titanic', 1997, N'Drama, Romance')
+INSERT INTO [dbo].[Movies] ([MovieId], [Title], [Year], [Genre]) VALUES (3, N'Top Gun', 1986, N'Action, Drama')
